b251124f02f638b2944067f597597f55bc26a6d1
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-07-16 01:09:33 +0300

    first class functions 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+let returnFirstTwoDrivers = function(arr) {
+  return arr.slice(0, 2);
+}
+
+let returnLastTwoDrivers = function(arr) {
+  return arr.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+let createFareMultiplier = function(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  }
+}
+
+let fareDoubler = createFareMultiplier(2);
+
+let fareTripler = createFareMultiplier(3);
+
+let selectDifferentDrivers = function(arrayOfDrivers, func) {
+  return func(arrayOfDrivers);
+}
